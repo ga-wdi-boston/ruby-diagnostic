730186b4a97dd088c551d28e05fb352dc221ee90
Tom Dyer <tdyer1@gmail.com>
2015-10-19 18:52:55 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+![General Assembly Logo](http://i.imgur.com/ke8USTq.png)
+
+## Setup Instructions
+
+Fork and clone this repository; then, follow the directions given in `challenge.rb`. You may use any resource, other than someone else in the classroom, to help you.
+
+## Submitting Your Solution
+
+Once you finish (or run out of time), push your code back up to your fork on GitHub and initiate a pull request.
+
---
+++ challenge.rb
@@ -0,0 +1,46 @@
+# Assessment :: Ruby Basics
+
+
+# Question 1 : Ruby objects and classes.
+# How would one find the class of the below objects.
+
+"Hello World"
+33
+65.89
+
+# Question 2: Booleans
+# What would the be the result of executing the below statements
+
+if 33 && 55
+  puts "it's true"
+else
+  puts "it's false"
+end
+
+# 2.1 Substitue "hello" && 4.5 for 33 && 55 above.
+# 2.2 Substitue nil && 88 for 33 && 55 above.
+# 2.4 Substitue 63.4 < 64
+
+# Question 3 :Strings
+
+# How would you (Yes, you can look up the String methods!):
+
+# 3.1. determine if a string included another another string?
+# 3.2. determine if the length of a string?
+# 3.3. changes the string "eels in my hovercraft" to "fish in my sink"
+#
+
+# Question 4 : Variables
+# Change the below string, don't make copies of the string.
+
+vehicle = "4 wheel Bus"
+
+# 4.1 to "6 wheel Bus"
+# 4.2 to "6 wheel tank"
+# 4.3 Create a new string from the string in 42 that have the value
+# "6 wheel tank that shoots flowers"
+
+# Question 5 : Loop
+# write a loop that guess your age.
+# Only allow 4 guesses
+# Notify the user if they guess correctly OR if they run out of turns to guess
